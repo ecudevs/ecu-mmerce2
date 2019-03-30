68eb66612d6d723f4d514eb6425ee3e24251036a
danmatlam <danmat_gd@hotmail.com>
2019-03-30 11:37:30 -0500

    primer push

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,52 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  datosFormulario:any = {
+    descripcion: 'Encebollado'
+  };
+
+  opciones = [
+    { valor: 'B', texto: 'Bebidas' },
+
+    { valor: 'A', texto: 'Alimento' },
+
+    { valor: 'T', texto: 'Tecnologia' }
+  ]
+
+  cervezas:any = [
+    {
+      descripcion: 'Cervezas',
+      precio: 2,
+      stock: 5,
+      categoria: 'BEBIDAS'
+    },
+    {
+      descripcion: 'Cervezas 1',
+      urlImage: 'https://http2.mlstatic.com/cerveza-corona-D_NQ_NP_979411-MLC20550406761_012016-F.jpg',
+      precio: 2,
+      stock: 5,
+      categoria: 'BEBIDAS'
+    },
+    {
+      descripcion: 'Cervezas 2',
+      urlImage: 'https://http2.mlstatic.com/cerveza-corona-D_NQ_NP_979411-MLC20550406761_012016-F.jpg',
+      precio: 2,
+      stock: 5,
+      categoria: 'BEBIDAS'
+    },
+    {
+      descripcion: 'Cervezas 2',
+      urlImage: 'https://http2.mlstatic.com/cerveza-corona-D_NQ_NP_979411-MLC20550406761_012016-F.jpg',
+      precio: 2,
+      stock: 5,
+      categoria: 'BEBIDAS'
+    }
+  ]
+
+  agregarProducto() {
+    this.cervezas.push(this.datosFormulario);
+    this.datosFormulario = {};
+  }
+}
